e6f28aff2189ce3c8fdff6f5ccd14f5a360a0e59
Рамиль Хаматшин <ramil_kh@Ramils-MacBook-Air.local>
2025-08-25 16:19:22 +0300

    build(config): add pinia module; add css, meta and pinia module configs into nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/image', '@pinia/nuxt'],
+  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: 'Квартиры',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
+    }
+  }
 })
